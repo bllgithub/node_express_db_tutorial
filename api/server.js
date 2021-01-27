a1974537d51a381c02300dabdc84806186eee784
Luisito Lozano <bllhumanityapps@gmail.com>
2021-01-27 22:13:38 +0800

    dgffdg

--- api/server.js
+++ api/server.js
@@ -1,5 +1,18 @@
 const express = require('express');
+
+// morgan allows us to log access to our resources in console log:
+// below are examples of logs:
+    // POST /api/auth/login 200 302.778 ms - 229
+    // GET /api/users 200 2.818 ms - 1180
+    // GET /api/messages 200 1.461 ms - 581
 const morgan = require('morgan');
+
+// helmet hides info in the response headers about "express" 
+const helmet = require('helmet');
+
+//cors allows the api and client to be able to talk to each other
+const cors = require('cors');
+
 const restricted = require('../auth/restricted-middleware');
 
 const lessonsRouter = require('../Routes/lessons-routes');
@@ -9,7 +22,9 @@ const authRouter = require('../auth/auth-routes');
 
 
 const server = express();
+server.use(helmet());
 server.use(morgan('dev'));
+server.use(cors());
 server.use(express.json());
 
 server.get('/', (req, res) => {
@@ -17,6 +32,7 @@ server.get('/', (req, res) => {
 })
 
 server.use('/api/auth', authRouter);
+// we can put more middleware here in between "restricted" and "lessonsRouter if we need to.
 server.use('/api/lessons', restricted, lessonsRouter);
 server.use('/api/messages', restricted, messagesRouter);
 server.use('/api/users', restricted, usersRouter);
